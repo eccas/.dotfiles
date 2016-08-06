1a2ad631d8b385a0bb4fe446849f04418d28697e
eccas <joakim.sjunnebo@gmail.com>
2016-08-06 21:11:12 +0100

    added new new config (spacemacs, zsh)

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+# Remember: base16-gnome-terminal
---
+++ touchpad/.touchpadsetup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Custom touchpad settings
+synclient PalmDetect=1
+synclient FingerHigh=30
+synclient MinSpeed=0.35
+synclient MaxSpeed=1.8
+synclient AccelFactor=0.08
+synclient AreaTopEdge=2500
+synclient AreaBottomEdge=0
+synclient RightButtonAreaTop=0
+synclient RightButtonAreaLeft=3292
+synclient MiddleButtonAreaRight=3292
+synclient MiddleButtonAreaLeft=2600
