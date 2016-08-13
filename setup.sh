746a269a3742c054ba7a6f25ad7ab23d3b6b6638
eccas <joakim.sjunnebo@gmail.com>
2016-08-13 11:04:02 +0100

    finalized setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# This will setup up a basic environment with emacs, zsh, terminal colors
+# and some additional settings. The old environment will be backed up to
+# $HOME/migrate.
+
+SETUPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Backup existing env
 mkdir -p $HOME/migrate
 MIGRATEDIR=$HOME/migrate
@@ -10,20 +18,22 @@ if [ -e $HOME/.gitignore_global ]; then mv $HOME/.gitignore_global $MIGRATEDIR ;
 if [ -e $HOME/.dir_colors       ]; then mv $HOME/.dir_colors       $MIGRATEDIR ; fi
 if [ -e $HOME/.zshrc            ]; then mv $HOME/.zshrc            $MIGRATEDIR ; fi
 
-# Install git
-sudo apt-get install git-all
-
 # Install emacs and spacemacs
 sudo apt-get install emacs24 emacs24-common emacs24-common-non-dfsg emacs24-el emacs24-dbg
 git clone https://github.com/syl20bnr/spacemacs $HOME/.emacs.d
 
 # Install zsh and oh-my-zsh
-sudo apt-get zsh zsh-doc
-zsh --version
-chsh -s $(which zsh)
+sudo apt-get install zsh zsh-doc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp zsh/af-magic.zsh-theme $HOME/.oh-my-zsh/custom/themes/
+mkdir -p $HOME/.oh-my-zsh/custom/themes
+cp $SETUPDIR/zsh/af-magic.zsh-theme $HOME/.oh-my-zsh/custom/themes/
 
 # Install base16-gnome-terminal
 git clone https://github.com/chriskempson/base16-gnome-terminal.git ~/.config/base16-gnome-terminal
 source .config/base16-gnome-terminal/base16-monokai.dark.sh
+
+# Install additional packages
+sudo apt-get install xsel
+
+# Setup symlinks
+source $SETUPDIR/symlink-setup.sh
